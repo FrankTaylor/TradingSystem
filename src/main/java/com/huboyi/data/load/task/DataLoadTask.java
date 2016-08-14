45138b19048abe9ebd731de543328b80eafea0b0
FrankTaylor <franktaylor@163.com>
2016-08-14 13:49:02 +0800

    哈哈

--- src/main/java/com/huboyi/data/load/task/DataLoadTask.java
+++ src/main/java/com/huboyi/data/load/task/DataLoadTask.java
@@ -32,7 +32,7 @@ import com.huboyi.data.load.DataLoadEngine;
  * 文件名及内容格式解释：
  * 1、文件名示例：SH600015.txt。其中，SH代表上证交易所、SZ代表深证交易所；600015则表示挣钱代码。
  * 2、内容的实例：20030912,1.20,1.25,1.04,1.06,480212200,3516635904.00
- *           20150616,0931,7.04,7.04,6.98,6.99,9179800,64864208.00
+ *           	 20150616,0931,7.04,7.04,6.98,6.99,9179800,64864208.00
  * 由于，不管什么证券类的软件都可以导出好几种格式的数据，为提高程序的适应性，在此规定导出数据的格式：“时间，时分，开，高，低，收，成交量，成交额”。
  * 在此还需要说明一下，如果导出的数据是日线，那就不会有“时分”这个列了。所以在上边给出的内容示例有两个，一个是7列，一个是8列。
  * 
@@ -125,108 +125,102 @@ public class DataLoadTask implements Callable<List<MarketDataBean>> {
 		// 装载读取行情数据Bean的集合。
 		List<StockDataBean> beanList = new CopyOnWriteArrayList<StockDataBean>();
 		
-		try {
-			BufferedReader reader = null;
-			try {
-				reader = new BufferedReader(new InputStreamReader(new FileInputStream(marketDataFilepath), "UTF-8"));
-				String s;
-				while ((s = reader.readLine()) != null) {
-					if (s.matches("[0-9]+.*")) {
-						String[] dataArray = s.split(separator);
-						
-						// 目前程序只能装载，日间和日内行情数据信息，对于月、周和毫秒类型的数据格式是不支持的。
-						if (dataArray != null && (dataArray.length == 7 || dataArray.length == 8)) {
-							
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(marketDataFilepath), "UTF-8"));) {
+
+			String s;
+			while ((s = reader.readLine()) != null) {
+				if (s.matches("[0-9]+.*")) {
+					String[] dataArray = s.split(separator);
+
+					// 目前程序只能装载，日间和日内行情数据信息，对于月、周和毫秒类型的数据格式是不支持的。
+					if (dataArray != null && (dataArray.length == 7 || dataArray.length == 8)) {
+
 							/* ---------------------- 一、读取数据 ---------------------- */
-							
-							// --- 时间信息 ---
-							String yearAndMonthAndDay = dataArray[0];                                                           // 年月日。
-							String hourAndMinute = null;                                                                        // 时分。
-							
-							// --- 价格信息 ---
-							String open = null;                                                                                 // 开盘价。
-							String high = null;                                                                                 // 最高价。
-							String low = null;                                                                                  // 最低价。
-							String close = null;                                                                                // 收盘价。
-							
-							// --- 成交信息 ---
-							String volume = null;                                                                               // 成交量。
-							String amount = null;                                                                               // 成交额。
-							
-							if (dataArray.length == 7) {                                                                        // 装载日线行情数据。 
-								hourAndMinute = null;
-								open = dataArray[1]; high = dataArray[2]; low = dataArray[3]; close = dataArray[4];
-								volume = dataArray[5]; amount = dataArray[6];
-							} else if (dataArray.length == 8) {                                                                 // 装载分钟行情数据。
-								hourAndMinute = dataArray[1];
-								open = dataArray[2]; high = dataArray[3]; low = dataArray[4]; close = dataArray[5];
-								volume = dataArray[6]; amount = dataArray[7];
-							}
-							
-							if (
-									StringUtils.isEmpty(yearAndMonthAndDay) || yearAndMonthAndDay.length() != 8 ||
-									(hourAndMinute != null && hourAndMinute.length() != 4) ||
-									
-									StringUtils.isEmpty(open) ||
-									StringUtils.isEmpty(high) ||
-									StringUtils.isEmpty(low) ||
-									StringUtils.isEmpty(close) ||
-									
-									StringUtils.isEmpty(volume) ||
-									StringUtils.isEmpty(amount)
-								) {
-								throw new RuntimeException("股票行情文件 [" + marketDataFilepath + "] 中的内容格式不符合程序的要求！");
-							}
-							
-							/* ---------------------- 二、装载数据 ---------------------- */
-							
-							StockDataBean bean = new StockDataBean();
-							
-							// --- 时间信息 ---
-							
-							bean.setYear(Integer.valueOf(yearAndMonthAndDay.substring(0, 4)));                                  // 年。
-							bean.setMonth(Integer.valueOf(yearAndMonthAndDay.substring(4, 6)));                                 // 月。
-							bean.setDay(Integer.valueOf(yearAndMonthAndDay.substring(6, 8)));                                   // 日。
-							bean.setHour((hourAndMinute != null) ? Integer.valueOf(hourAndMinute.substring(0, 2)) : 0);         // 时。
-							bean.setMinute((hourAndMinute != null) ? Integer.valueOf(hourAndMinute.substring(2, 4)) : 0);       // 分。
-							bean.setSecond(0);                                                                                  // 秒。
-							bean.setMillisecond(0);                                                                             // 毫秒。
-							
-							bean.setDate(Long.valueOf(                                                                          // 日期（格式：yyyyMMddhhmmssSSS）。
-									yearAndMonthAndDay
-									.concat((hourAndMinute != null ? hourAndMinute : "0000"))
-									.concat((bean.getSecond() > 0 ? String.valueOf(bean.getSecond()) : "00"))
-									.concat((bean.getMillisecond() > 0 ? String.valueOf(bean.getMillisecond()) : "000"))));
-							bean.setTime(dataFormat.parse(String.valueOf(bean.getDate())).getTime());                           // 时间（格式为当前计算机时间和GMT时间(格林威治时间)1970年1月1号0时0分0秒所差的毫秒数）。
-							
-							// --- 价格信息 ---
-							bean.setOpen(BigDecimal.valueOf(Float.valueOf(open)).setScale(3, RoundingMode.HALF_UP));            // 开盘价。
-							bean.setHigh(BigDecimal.valueOf(Float.valueOf(high)).setScale(3, RoundingMode.HALF_UP));            // 最高价。
-							bean.setLow(BigDecimal.valueOf(Float.valueOf(low)).setScale(3, RoundingMode.HALF_UP));              // 最低价。
-							bean.setClose(BigDecimal.valueOf(Float.valueOf(close)).setScale(3, RoundingMode.HALF_UP));          // 收盘价。
-							
-							// --- 成交信息 ---
-							bean.setVolume(BigDecimal.valueOf(Float.valueOf(volume)).setScale(2, RoundingMode.HALF_UP));        // 成交量。
-							bean.setAmount(BigDecimal.valueOf(Float.valueOf(amount)).setScale(3, RoundingMode.HALF_UP));        // 成交额。
-							
-							// --- 构造链表 ---
-							if (beanList.size() > 0) {
-		                    	StockDataBean prev = beanList.get(beanList.size() - 1);
-		                    	if (null != prev) {
-		                    		bean.setPrev(prev);
-		                    		prev.setNext(bean);
-		                    	}
-		                    }
-		                    
-		                    beanList.add(bean);
+
+						// --- 时间信息 ---
+						String yearAndMonthAndDay = dataArray[0];                                                           // 年月日。
+						String hourAndMinute = null;                                                                        // 时分。
+
+						// --- 价格信息 ---
+						String open = null;                                                                                 // 开盘价。
+						String high = null;                                                                                 // 最高价。
+						String low = null;                                                                                  // 最低价。
+						String close = null;                                                                                // 收盘价。
+
+						// --- 成交信息 ---
+						String volume = null;                                                                               // 成交量。
+						String amount = null;                                                                               // 成交额。
+
+						if (dataArray.length == 7) {                                                                        // 装载日线行情数据。
+							hourAndMinute = null;
+							open = dataArray[1]; high = dataArray[2]; low = dataArray[3]; close = dataArray[4];
+							volume = dataArray[5]; amount = dataArray[6];
+						} else if (dataArray.length == 8) {                                                                 // 装载分钟行情数据。
+							hourAndMinute = dataArray[1];
+							open = dataArray[2]; high = dataArray[3]; low = dataArray[4]; close = dataArray[5];
+							volume = dataArray[6]; amount = dataArray[7];
 						}
+
+						if (
+								StringUtils.isEmpty(yearAndMonthAndDay) || yearAndMonthAndDay.length() != 8 ||
+										(hourAndMinute != null && hourAndMinute.length() != 4) ||
+
+										StringUtils.isEmpty(open) ||
+										StringUtils.isEmpty(high) ||
+										StringUtils.isEmpty(low) ||
+										StringUtils.isEmpty(close) ||
+
+										StringUtils.isEmpty(volume) ||
+										StringUtils.isEmpty(amount)
+								) {
+							throw new RuntimeException("股票行情文件 [" + marketDataFilepath + "] 中的内容格式不符合程序的要求！");
+						}
+
+							/* ---------------------- 二、装载数据 ---------------------- */
+
+						StockDataBean bean = new StockDataBean();
+
+						// --- 时间信息 ---
+
+						bean.setYear(Integer.valueOf(yearAndMonthAndDay.substring(0, 4)));                                  // 年。
+						bean.setMonth(Integer.valueOf(yearAndMonthAndDay.substring(4, 6)));                                 // 月。
+						bean.setDay(Integer.valueOf(yearAndMonthAndDay.substring(6, 8)));                                   // 日。
+						bean.setHour((hourAndMinute != null) ? Integer.valueOf(hourAndMinute.substring(0, 2)) : 0);         // 时。
+						bean.setMinute((hourAndMinute != null) ? Integer.valueOf(hourAndMinute.substring(2, 4)) : 0);       // 分。
+						bean.setSecond(0);                                                                                  // 秒。
+						bean.setMillisecond(0);                                                                             // 毫秒。
+
+						bean.setDate(Long.valueOf(                                                                          // 日期（格式：yyyyMMddhhmmssSSS）。
+								yearAndMonthAndDay
+										.concat((hourAndMinute != null ? hourAndMinute : "0000"))
+										.concat((bean.getSecond() > 0 ? String.valueOf(bean.getSecond()) : "00"))
+										.concat((bean.getMillisecond() > 0 ? String.valueOf(bean.getMillisecond()) : "000"))));
+						bean.setTime(dataFormat.parse(String.valueOf(bean.getDate())).getTime());                           // 时间（格式为当前计算机时间和GMT时间(格林威治时间)1970年1月1号0时0分0秒所差的毫秒数）。
+
+						// --- 价格信息 ---
+						bean.setOpen(BigDecimal.valueOf(Float.valueOf(open)).setScale(3, RoundingMode.HALF_UP));            // 开盘价。
+						bean.setHigh(BigDecimal.valueOf(Float.valueOf(high)).setScale(3, RoundingMode.HALF_UP));            // 最高价。
+						bean.setLow(BigDecimal.valueOf(Float.valueOf(low)).setScale(3, RoundingMode.HALF_UP));              // 最低价。
+						bean.setClose(BigDecimal.valueOf(Float.valueOf(close)).setScale(3, RoundingMode.HALF_UP));          // 收盘价。
+
+						// --- 成交信息 ---
+						bean.setVolume(BigDecimal.valueOf(Float.valueOf(volume)).setScale(2, RoundingMode.HALF_UP));        // 成交量。
+						bean.setAmount(BigDecimal.valueOf(Float.valueOf(amount)).setScale(3, RoundingMode.HALF_UP));        // 成交额。
+
+						// --- 构造链表 ---
+						if (beanList.size() > 0) {
+							StockDataBean prev = beanList.get(beanList.size() - 1);
+							if (null != prev) {
+								bean.setPrev(prev);
+								prev.setNext(bean);
+							}
+						}
+
+						beanList.add(bean);
 					}
 				}
-			} finally {
-				if (null != reader) {
-					reader.close();
-				}
 			}
+
 		} catch (Exception e) {
 			log.error(e);
 		}
